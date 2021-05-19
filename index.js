888a96c349e131130753774492b371310c0988fc
Olha10 <100kravchenko@gmail.com>
2021-05-19 21:00:31 +0300

    lesson1

---
+++ index.js
@@ -0,0 +1,39 @@
+const message = "Hello world";
+console.log(message);
+//comment
+/*comment
+*/
+let food = "Banana";
+console.log (food);
+
+let country = "UK";
+country = "USA";
+console.log (country);
+
+//Numbers
+let numb = 10;
+numb = "10";
+console.log (numb);
+
+console.log (-1/0);
+console.log ('Banana'/2+2);
+
+//String
+let qa = "QA";
+let name = 'Vasya';
+let phrase = `Super ${name + qa}`;
+console.log (phrase);
+
+let phrase2 = `Super ${qa + name} in our project`;
+console.log (phrase2);
+
+//Null (empty box)
+let nothing = null;
+console.log (nothing);
+
+let emptiness;
+console.log (emptiness);
+
+console.log (typeof console);
+
+
